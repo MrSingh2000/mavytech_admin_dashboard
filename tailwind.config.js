8f6e3f566f456e0ee932e5a62d33e0c92bb5f391
Anshuman Singh <77919788+MrSingh2000@users.noreply.github.com>
2024-11-13 23:34:41 +0530

    Revert "added dashborad graphs (#4)" (#6)
    This reverts commit 785ca49d99ef59fc196d1d77e1bb703aaad339fe.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,69 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			textMain: '#464255',
-  			textLight: '#A3A3A3',
-  			bgGrey: '#F3F2F7',
-  			success: '#00B074',
-  			success20percent: '#00B07426',
-  			blueCyan: '#2D9CDB26',
-  			grey: '#D0D6DE',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        textMain: '#464255',
+        textLight: '#A3A3A3',
+        bgGrey: '#F3F2F7',
+        success: '#00B074',
+        success20percent: '#00B07426',
+        blueCyan: '#2D9CDB26',
+        grey: '#D0D6DE'
+      },
+    },
   },
   plugins: [
-      require("tailwindcss-animate")
-],
+    
+  ],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +8,4 @@ export default defineConfig({
     port: 3000, // Change to your desired port
   },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
